73e18fe814fe6d8183d8d61bb768b272c4b884a4
Vanessa Aryanata <vanessa.aryanata@datatrans.ch>
2022-12-22 14:25:01 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <ThemedHtml>
         <head>
           <title>Curriculum Vitae - Vanessa Aryanata</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="theme-color" content="#ffffff"></meta>
         </head>
         <body className='overflow-y-scroll dark:bg-zinc-900'>
           <div className='grid grid-cols-[1fr,minmax(auto,240px),min(800px,100%),1fr] gap-x-8 py-8'>
